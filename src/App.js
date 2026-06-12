057b8a2f20b4d32cbd063895b14173ec3bc07555
Rukshanmohottige <rukshanmohottige@gmail.com>
2026-06-12 13:00:16 +0530

    Switch to HashRouter to fix GitHub Pages refresh 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/MyPortfolio__">
+    <Router>
       <div className="App">
         <Navbar />
         
