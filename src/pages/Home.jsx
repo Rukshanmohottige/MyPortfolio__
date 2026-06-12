4827b48a447e3ae4ca7f57aaa02d11b291246b36
Rukshanmohottige <rukshanmohottige@gmail.com>
2026-06-12 12:56:59 +0530

    Rename image to profile.png to fix GitHub Pages spaces issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
           </div>
         </div>
         <div className="hero-image">
-          <img src={process.env.PUBLIC_URL + '/Screenshot 2026-06-12 024511.png'} alt="Rukshan Mohottige" />
+          <img src={process.env.PUBLIC_URL + '/profile.png'} alt="Rukshan Mohottige" />
         </div>
       </div>
     </section>
